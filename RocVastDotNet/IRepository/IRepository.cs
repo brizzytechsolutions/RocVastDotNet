ac9adf67031ab9345b036f725325a9e29a8c3603
kabzangwana <79355407+kabzangwana@users.noreply.github.com>
2024-02-13 20:29:53 +0200

    creating repostiries

---
+++ RocVastDotNet/IRepository/IRepository.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RocVastDotNet.Interfaces
+{
+    public interface IRepository<T> where T : class
+    {
+        Task<IEnumerable<T>> GetAllAsync();
+        Task<T> GetByIdAsync(int id);
+        Task AddAsync(T entity);
+    }
+}
---
+++ RocVastDotNet/Repository/Repository.cs
@@ -0,0 +1,57 @@
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Threading.Tasks;
+
+namespace RocVastDotNet.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        protected readonly ProductContext Context;
+
+        public Repository(ProductContext context)
+        {
+            Context = context;
+        }
+
+        public async Task<IEnumerable<T>> GetAllAsync()
+        {
+            return await Context.Set<T>().ToListAsync();
+        }
+
+        public async Task<T> GetByIdAsync(int id)
+        {
+            return await Context.Set<T>().FindAsync(id);
+        }
+
+        public async Task<T> FirstOrDefaultAsync(Expression<Func<T, bool>> predicate)
+        {
+            return await Context.Set<T>().FirstOrDefaultAsync(predicate);
+        }
+
+        public async Task AddAsync(T entity)
+        {
+            await Context.Set<T>().AddAsync(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task UpdateAsync(T entity)
+        {
+            Context.Set<T>().Update(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public async Task RemoveAsync(T entity)
+        {
+            Context.Set<T>().Remove(entity);
+            await Context.SaveChangesAsync();
+        }
+
+        public IEnumerable<T> Find(Expression<Func<T, bool>> predicate)
+        {
+            return Context.Set<T>().Where(predicate);
+        }
+    }
+}
